a5bad3a1eb4c92ac0b7c4bafa3bab838249774b5
keiranoluv <104440961+keiranoluv@users.noreply.github.com>
2026-06-08 20:38:52 +0700

    Time: 22 ms (86.14%) | Memory: 33.6 MB (91.20%) - LeetSync

---
+++ 2265-partition-array-according-to-given-pivot/partition-array-according-to-given-pivot.py
@@ -0,0 +1,16 @@
+class Solution:
+    def pivotArray(self, nums: List[int], pivot: int) -> List[int]:
+        smaller = []
+        greater = []
+        equal = []
+
+        for num in nums:
+            if num<pivot:
+                smaller.append(num)
+            elif num>pivot:
+                greater.append(num)
+            else:
+                equal.append(num)
+
+        return smaller + equal + greater
+        
